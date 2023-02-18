2ce0115c805395741cee60a3c8f4516858e39e1f
Julyidol <lineoff@mail.com>
2023-02-19 02:02:36 +0300

    Добавили приветствие Маши

---
+++ Example006_ifElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите Ваше имя ");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+Console.WriteLine("Ура! Маша тут");
+}
+else
+{
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+}
